71bff1776126f7ed973099c9cc0286af700e0d9d
Sylwester Lachiewicz <slachiewicz@apache.org>
2021-12-06 16:24:14 +0100

    [MSHARED-1003] Deps cleanup

--- src/test/java/org/apache/maven/shared/filtering/MultiDelimiterInterpolatorFilterReaderLineEndingTest.java
+++ src/test/java/org/apache/maven/shared/filtering/MultiDelimiterInterpolatorFilterReaderLineEndingTest.java
@@ -20,8 +20,8 @@ package org.apache.maven.shared.filtering;
  */
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isA;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
 
 import java.io.Reader;

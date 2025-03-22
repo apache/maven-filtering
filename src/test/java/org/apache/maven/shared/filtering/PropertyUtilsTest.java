db1bc162866da8ebc3f6c3b454a566946ed5188d
Sylwester Lachiewicz <slachiewicz@apache.org>
2025-03-23 00:38:22 +0100

    fix

--- src/test/java/org/apache/maven/shared/filtering/PropertyUtilsTest.java
+++ src/test/java/org/apache/maven/shared/filtering/PropertyUtilsTest.java
@@ -29,7 +29,9 @@ import java.util.Properties;
 import org.junit.jupiter.api.Test;
 
 import static org.codehaus.plexus.testing.PlexusExtension.getBasedir;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * @author Olivier Lamy

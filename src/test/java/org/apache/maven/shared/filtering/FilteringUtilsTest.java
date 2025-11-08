3f3906a0ad6445488ba192bbfb7f431bad39a1db
Fridrich Štrba <fridrich.strba@bluewin.ch>
2025-11-08 16:09:34 +0100

    Add test for filtering of binary files

--- src/test/java/org/apache/maven/shared/filtering/FilteringUtilsTest.java
+++ src/test/java/org/apache/maven/shared/filtering/FilteringUtilsTest.java
@@ -28,6 +28,7 @@ import java.nio.file.StandardCopyOption;
 import org.junit.jupiter.api.Test;
 
 import static org.apache.maven.api.di.testing.MavenDIExtension.getBasedir;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -145,5 +146,24 @@ class FilteringUtilsTest {
         assertEquals(
                 "jdbc:derby:C:\\\\Users\\\\Administrator/test;create=true",
                 FilteringUtils.escapeWindowsPath("jdbc:derby:C:\\Users\\Administrator/test;create=true"));
+    }
+
+    @Test
+    public void testDoNotThrowOnBinaryFile() {
+        Path fromFile = Paths.get(getBasedir() + "/src/test/units-files/binary-file/binary-file");
+        Path toFile = TEST_DIRECTORY.resolve("binary-file-copied");
+        assertDoesNotThrow(() -> FilteringUtils.copyFile(
+                fromFile,
+                toFile,
+                "UTF-8",
+                new FilterWrapper[] {
+                    new FilterWrapper() {
+                        @Override
+                        public Reader getReader(Reader fileReader) {
+                            return fileReader;
+                        }
+                    }
+                },
+                false));
     }
 }

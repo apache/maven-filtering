2ebc73618eeac0f20f8d295e202cda5b0419d76e
Guillaume Nodet <gnodet@gmail.com>
2026-09-26 19:08:43 +0200

    fix: remove sensitive property values from debug log output (MRESOURCES-290)
    - Replace full key=value property dump in BaseFilter.getDefaultFilterWrappers() with a count-only message
    - Prevents env.* namespace (AWS keys, GitHub tokens, database URLs) from leaking in `mvn -X` debug output
    - Retains diagnostic value: count still indicates whether properties were loaded correctly
    - Fixes apache/maven-resources-plugin#133 (MRESOURCES-290)

--- src/main/java/org/apache/maven/shared/filtering/BaseFilter.java
+++ src/main/java/org/apache/maven/shared/filtering/BaseFilter.java
@@ -27,7 +27,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.TreeSet;
 import java.util.function.Consumer;
 
 import org.apache.maven.api.Project;
@@ -149,10 +148,7 @@ class BaseFilter implements DefaultFilterInfo {
                 new ArrayList<>(request.getDelimiters().size() + 1);
 
         if (getLogger().isDebugEnabled()) {
-            getLogger().debug("properties used:");
-            for (String s : new TreeSet<>(filterProperties.stringPropertyNames())) {
-                getLogger().debug(s + ": " + filterProperties.getProperty(s));
-            }
+            getLogger().debug("number of properties used for filtering: {}", filterProperties.size());
         }
 
         final ValueSource propertiesValueSource = new PropertiesBasedValueSource(filterProperties);
